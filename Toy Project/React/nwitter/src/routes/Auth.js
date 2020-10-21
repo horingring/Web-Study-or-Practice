4d9fa885cff370d67de9bb807c491942335aa2f9
Horingring <dldldksl@naver.com>
2020-10-21 21:25:30 +0900

    1021_Nwitter_2.5._Social LogIn (unresolved error)
    에러 있음.
    콘솔에
    Uncaught Error: The error you provided does not contain a stack trace.
    라는 에러가 뜸.
    아직 해결하지 못함.

--- Toy Project/React/nwitter/src/routes/Auth.js
+++ Toy Project/React/nwitter/src/routes/Auth.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import { authService } from "fbase";
+import { authService, firebaseInstance } from "fbase";
 
 const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState("");
   const onChange = (event) => {
     const {
       target: { name, value },
@@ -29,9 +30,35 @@ const Auth = () => {
       }
       console.log(data);
     } catch (error) {
-      console.log(error);
+      setError(error.message);
     }
   };
+  const toggleAccount = () => setNewAccount((prev) => !prev);
+  const onSocialClick = async (event) => {
+    const {
+      target: { name },
+    } = event;
+    let provider;
+    if (name === "google") {
+      provider = new firebaseInstance.auth.GoogleAuthProvider();
+    } else if (name === "github") {
+      provider = new firebaseInstance.auth.GithubAuthProvider();
+    }
+    const data = await authService.signInWithPopup(provider);
+    console.log(data);
+    // const {
+    //   target: { name },
+    // } = event;
+    // let provider;
+    // if (name === "google") {
+    //   provider = new firebaseInstance.auth.GoogleAuthProvider();
+    // } else if (name === "github") {
+    //   provider = new firebaseInstance.auth.GithubAuthProvider();
+    // }
+    // const data = await authService.signInWithPopup(provider);
+    // console.log(data);
+  };
+
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -55,10 +82,18 @@ const Auth = () => {
           type="submit"
           value={newAccount ? "Create Account" : "Log In"}
         ></input>
+        {error}
       </form>
+      <span onClick={toggleAccount}>
+        {newAccount ? "Sign In" : "Create Account"}
+      </span>
       <div>
-        <button>Continue with Google</button>
-        <button>Continue with Github</button>
+        <button name="google" onClick={onSocialClick}>
+          Continue with Google
+        </button>
+        <button name="github" onClick={onSocialClick}>
+          Continue with Github
+        </button>
       </div>
     </div>
   );
