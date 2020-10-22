e1a8da204feb2bc6284c44405ee0e2fb80ad163a
Horingring <dldldksl@naver.com>
2020-10-22 15:33:27 +0900

    1022_Nwitter_3.0, 3.1_Database Setup , Nwitting
    3.0.
    1. Made Forms
    2. Database Setup
    
    3.1. Nwitting

--- Toy Project/React/nwitter/src/routes/Home.js
+++ Toy Project/React/nwitter/src/routes/Home.js
@@ -1,5 +1,35 @@
-import React from 'react';
+import React, { useState } from "react";
+import { dbService } from "fbase";
 
-const Home = () => <span>Home</span>
-
+const Home = () => {
+  const [nweet, setNweet] = useState("");
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await dbService.collection("nweets").add({
+      nweet,
+      createdAt: Date.now(),
+    });
+    setNweet("");
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNweet(value);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={nweet}
+          type="text"
+          placeholder="What's on your ind?"
+          maxLength={120}
+        ></input>
+        <input type="submit" value="Nweet" />
+      </form>
+    </div>
+  );
+};
 export default Home;
