b71b5373f8b757c6c634aa95a686170b05e6e217
Horingring <dldldksl@naver.com>
2020-10-22 19:15:09 +0900

    1022_Nwitter_3.2._Getting the Nweets
    -firestore로부터 데이터 가져오기(get)
    -forEach와 map 함수의 차이점 유의.
    *forEach는 배열의 각 index 수만큼 명령을 실행(각 index 매개변수로 받을 수 있음)
    *map은 각 index 수만큼 명령을 실행한 뒤 return된 것으로 새로운 배열을 생성(이 역시 각 index를 매개변수로 받을 수 있음)

---
+++ Study & Practice/React/Web Study/react-study_by-book/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  const name = undefined;
+  return <div className="react">{name || "Undefined입니다."}</div>;
+}
+
+export default App;
