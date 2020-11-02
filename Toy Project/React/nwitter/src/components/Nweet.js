35cb7ed52ab6b9abe234493db2c7c34df5aacca8
Horingring <dldldksl@naver.com>
2020-11-02 23:38:42 +0900

    1102_Nwitter_4.4._Deleting Files
    - uuidv4( ); 를 통해 img의 name을 설정하였으므로, 삭제를 하고싶지만 해당 파일을 직접 지정할 수 없다.
    - 그러나 그 대신, refFromURL( url ); 을 통해 지정할 수 있다. 그 url은 바로 collection에 저장된 url주소에서 가져온 것이다.
    - 여기의 경우 Nweet.js에 props로 넘겨준 nweetObj.attachmentUrl 을 통해 접근이 가능하다.
    - 그렇게 접근하여 .delete( ); 를 해주면 storage에 저장되어 있는 파일까지 삭제할 수 있게 된다.

--- Toy Project/React/nwitter/src/components/Nweet.js
+++ Toy Project/React/nwitter/src/components/Nweet.js
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
-import { dbService } from "fbase";
+import { dbService, storageService } from "fbase";
 
 const Nweet = ({ nweetObj, isOwner }) => {
   const [editing, setEditing] = useState(false);
   const [newNweet, setNewNweet] = useState(nweetObj.text);
-  const onDeleteClick = () => {
+  const onDeleteClick = async () => {
     const ok = window.confirm("Are you sure you want to delete this nweet?");
     if (ok) {
-      dbService.doc(`nweets/${nweetObj.id}`).delete();
+      await dbService.doc(`nweets/${nweetObj.id}`).delete();
+      await storageService.refFromURL(nweetObj.attachmentUrl).delete();
+      alert("삭제되었습니다");
     }
   };
   const toggleEditing = () => {
