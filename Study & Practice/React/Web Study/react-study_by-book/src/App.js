d7ffe11974e34761039bf98a5c5297a98a6d2eb7
Horingring <dldldksl@naver.com>
2020-11-19 22:16:26 +0900

    1119_ReactStudy_ByBook_5.3_컴포넌트에 ref 달기
    -컴포넌트에도 ref를 달 수 있다.
    - 이 방법은 주로 외부에서 해당컴포넌트 내부에 있는 DOM(DOM 요소, 함수 등)을 사용할 때 쓴다.
    - 방법은 동일하다.
      예) App에서 ScrollBox 컴포넌트를 사용할 때
    <div>
      <ScrollBox ref={(ref)=>{this.scrollBoxxx=ref}} />
      <button onClick={
         ( ) => {this.scrollBoxxx.scrollToBottom( )} } >
      맨 밑으로</button>
    </div>
    
    **서로 다른 컴포넌트 간 데이터를 교류할 때 유의사항**
    => 반드시 부모 <---> 자식 의 흐름으로 데이터를 교류할 것!
    (*부모-자식 간이 아닌 형제 간 등의 컴포넌트 사용은 앱 규모가 커질 시 유지/보수가 매우 힘들어지기 때문)

--- Study & Practice/React/Web Study/react-study_by-book/src/App.js
+++ Study & Practice/React/Web Study/react-study_by-book/src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
-import ValidationSample from './ValidationSample';
-import CreateRefSample from './CreateRefSample';
+import ScrollBox from './ScrollBox';
 
 class App extends Component {
   render() {
     return (
-      <>
-        <ValidationSample />
-        <CreateRefSample />
-      </>
+      <div>
+        <ScrollBox
+          ref={(ref) => {
+            this.scrollBox = ref;
+          }}
+        />
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+      </div>
     );
   }
 }
