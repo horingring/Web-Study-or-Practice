4fd5bd5affe6ca3928eedff0e0f37c48c5df44a5
Horingring <dldldksl@naver.com>
2020-12-02 22:49:26 +0900

    1202_ReactStudy_ByBook_9.3.1_classnames 라이브러리
    1. CSS Module 사용 시 불편함을 해소
      - CSS Module을 사용할 때, 한 엘리먼트에 className을 2개 이상 걸어줄 경우, 템플릿 리터럴(Templete Literal)을 사용하는 것은 불편하다.
        => 1) classNames( 'param1', 'param2', ..);
                 이렇게 보다 가독성 좋게 쓸 수 있다.
        => 2) classNames.bind( ... ); 를 통해, CSS Module을 사용 시 .module.css를 import한 객체를 계속 쓰지 않아도 된다.
                 예시)
                 const hello = classNames.bind(styles);    // 이렇게 한 번 선언 후,
                 <div className={hello('wrapper', inverted')}>.. 
    
    2. 조건부로 클래스를 설정할 때 유용
    - p.238-239 참고

--- Study & Practice/React/Web Study/react-study_by-book_styling-react/src/CSSModule.js
+++ Study & Practice/React/Web Study/react-study_by-book_styling-react/src/CSSModule.js
@@ -1,10 +1,12 @@
 import React from "react";
+import classNames from "classnames/bind";
 import styles from "./CSSModule.module.css";
+
+const cx = classNames.bind(styles);
 
 const CSSModule = () => {
   return (
-    //하나일 때 -> className={styles.wrapper}
-    <div className={`${styles.wrapper} ${styles.inverted}`}>
+    <div className={cx("wrapper", "inverted")}>
       안녕하세요, 저는 <span className="something">CSS Module!</span>
     </div>
   );
