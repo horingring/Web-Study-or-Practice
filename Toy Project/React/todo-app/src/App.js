563eef669e4af4dda92d032c2604af1359a714c8
Horingring <dldldksl@naver.com>
2021-02-08 18:53:28 +0900

    0208_ReactStudy_ByBook_10.3_기능 구현하기_10.3.1_App에서 todos 상태 사용하기
    - App 컴포넌트에서 상태(state)관리
    - state의 데이터를 TodoList에 props로 전달
    - classnames 라이브러리를 사용해 조건부 className을 부여

--- Toy Project/React/todo-app/src/App.js
+++ Toy Project/React/todo-app/src/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '오늘의 운동 하기',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '삼시세끼 잘 챙겨먹기',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: '밤에 일찍 자기',
+      checked: false,
+    },
+  ]);
   return (
     <TodoTemplate>
       <TodoInsert />
-      <TodoList />
+      <TodoList todos={todos} />
     </TodoTemplate>
   );
 };
